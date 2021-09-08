40d21a0a194e5115ca0ebffabdbc1bcef36e394c
Quattro-Bajeena <motmen001@gmail.com>
2021-09-08 22:12:33 +0200

    minor html changes

--- XenoBooru.Web/wwwroot/js/file_downloads.js
+++ XenoBooru.Web/wwwroot/js/file_downloads.js
@@ -2,11 +2,14 @@
     const name = event.target.id;
     let options = {
         method: 'POST'
-    }
+    };
     fetch("/Home/RegisterFileDownload/?name=" + name, options)
         .then(response => response.json())
         .then(data => {
             console.log("Downloaded " + name);
+        })
+        .catch(error => {
+            console.log("this is error:", error);
         });
 }
 
